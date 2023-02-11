d20d12be9951f5fcbb111e1cf4086efabc4a85b4
kurves <kavssimply@gmail.com>
2023-02-11 17:14:25 +0300

    fix dependency errors by switching ts-loader version

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,13 +1,14 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+//import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth');
+let cachedCertificate : string
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
@@ -56,12 +57,14 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  //const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
+  const cert= await getCertificate()
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  logger.info(`Verifying token ${token}`)
+  return   verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +77,32 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+async function getCertificate(): Promise<string> {
+  logger.info(`Fetching certificate from ${jwksUrl}`)
+
+  const response = await Axios.get(jwksUrl)
+  const keys = response.data.keys
+
+  if (!keys || !keys.length)
+    throw new Error('No JWKS keys found')
+
+  const signingKeys = keys.filter(
+    key => key.use === 'sig'
+           && key.kty === 'RSA'
+           && key.alg === 'RS256'
+           && key.n
+           && key.e
+           && key.kid
+           && (key.x5c && key.x5c.length)
+  )
+
+  if (!signingKeys.length)
+    throw new Error('No JWKS signing keys found')
+
+
+  // Certificate found!
+  logger.info('Valid certificate found', cachedCertificate)
+
+  return cachedCertificate
 }
