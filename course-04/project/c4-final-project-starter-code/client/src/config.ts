b456794ada93fbfcc0299bb05f58585774de0eee
kurves <kavssimply@gmail.com>
2023-03-23 20:16:55 +0300

    configure api endpoint

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'https://mkppqaqtz5.execute-api.us-east-1.amazonaws.com/dev/todos'
+const apiId = 'qg9r598zze'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-12puib0un0xelcvv.us.auth0.com',            // Auth0 domain
-  clientId: '9fyN15jNdAWhLUTrenpQyJ3rWxeROzMc',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  clientId: 'IheIY5SUgA25HGm9JocaR6sW5K1p4o1l',          // Auth0 client id
+  callbackUrl: 'https://localhost:3000/callback'
 }
