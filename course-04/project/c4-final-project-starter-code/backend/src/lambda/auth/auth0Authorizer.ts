674286576947c8b0fb45b50e602ee95794aa1f89
kurves <kavssimply@gmail.com>
2023-03-24 17:04:17 +0300

    implement authentication

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,10 @@
 //import { APIGatewayAuthorizerEvent,CustomAuthorizerEvent, 
 import {CustomAuthorizerResult, CustomAuthorizerEvent} from 'aws-lambda'
 import 'source-map-support/register'
-import { verify} from 'jsonwebtoken'
+import { verify,decode} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-//import { Jwt } from '../../auth/Jwt'
+import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth');
@@ -14,10 +14,6 @@ const logger = createLogger('auth');
 const jwksUrl = 'https://dev-12puib0un0xelcvv.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
-  if (event.headers.authorization == "secretToken"){
-console.log('allowed');
-  }
-
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
@@ -57,25 +53,29 @@ console.log('allowed');
 }
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  
-  try{
+   
     const token = getToken(authHeader)
+    const jwt: Jwt = decode(token, { complete: true}) as Jwt
     //co const res = await Axios.get(jwksUrl);nst jwt: Jwt = decode(token, { complete: true }) as Jwt
     const res = await Axios.get(jwksUrl);
+    const keys = res.data.keys
+    const loginKeys = keys.find(key => key.kid ===jwt.header.kid)
+    logger.info('loginkeys', loginKeys)
+    if(!loginKeys){
+        throw new Error('The endpoint did not contain any keys')
+    }
     // TODO: Implement token verification
     // You should implement it similarly to how it was implemented for the exercise for the lesson 5
     // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-    const pem = res['data']['keys'][0]['x5c'][0]
+    const pem = loginKeys.x5c[0]
+    //cert
     const cert=`-----BEGIN CERTIFICATE-----\n${pem}\n-----END CERTIFICATE-----`
     logger.info(`Verifying token ${token}`)
-    return   verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
-
-  } catch(err){
-    logger.error('Failed to authencticate',err)
+    const verified = verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+    logger.info('Verified token', verified)
+    return verified
   } 
   
-}
-
 function getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
 
